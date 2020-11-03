b11a4eea57b1ba7620348dad3f9b97b2de05def1
Dan Frank <danielhfrank@gmail.com>
2020-11-02 16:16:04 -0800

    Closing serializers (#2)
    * Add closing functionality and change registry.open to a context manager
    
    * Rename
    
    * Add ZipSerializable
    
    * Finish ZipSerializable with a test
    
    * comment
    
    Co-authored-by: Dan Frank <df@facet.ai>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+from io import BytesIO
 import tempfile
+import zipfile
 
 from hashfs import HashFS
 import pytest
@@ -12,3 +14,13 @@ def fs_instance():
     with tempfile.TemporaryDirectory() as tmpdir:
         fs = HashFS(tmpdir, depth=1, width=2)
         yield fs
+
+
+@pytest.fixture
+def zip_addr(fs_instance):
+    buf = BytesIO()
+    zf = zipfile.ZipFile(buf, mode='w')
+    zf.writestr('df.txt', 'roolz')
+    zf.close()
+    buf.seek(0)
+    return fs_instance.put(buf)
---
+++ tests/serializable_test.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from .dataset import ZipSerializable
+from .opaque_example import OpaqueObject, OpaqueSerializable
+
+from cas_manifest.ref import Ref
+from cas_manifest.registry import SerializableRegistry
+
+
+def test_serde(fs_instance):
+    obj = OpaqueObject()
+
+    addr = OpaqueSerializable.dump(obj, fs_instance)
+
+    registry = SerializableRegistry(fs=fs_instance, classes=[OpaqueSerializable])
+    with registry.open(addr.id) as loaded_obj:
+        assert(obj.a == loaded_obj.a)
+
+
+def test_zip_serializable(zip_addr, fs_instance):
+    # First, take a pre-built zipfile (from contents in memory) and save to the fs
+    # See conftest.py for construction of zip_addr
+    zs = ZipSerializable(path=Ref(zip_addr.id))
+    zs_addr = zs.self_dump(fs_instance)
+
+    registry: SerializableRegistry[Path] = \
+        SerializableRegistry(fs=fs_instance, classes=[ZipSerializable])
+
+    # Extract the zipfile to a tmpdir path
+    with registry.open(zs_addr.id) as tmpdir_path:
+        # And assert that its contents are as expected
+        assert_zip_contents(tmpdir_path)
+
+        # Now, use ZipSerializable.dump to exercise a different path for saving the zipfile
+        zs_addr_2 = ZipSerializable.dump(tmpdir_path, fs_instance)
+
+    # And now extract that again and check that the contents are as expected
+    # The hashes will be different due to differences in e.g. metadata of file creation time
+    with registry.open(zs_addr_2.id) as tmpdir_path:
+        assert_zip_contents(tmpdir_path)
+
+
+def assert_zip_contents(tmpdir_path: Path):
+    assert(tmpdir_path.is_dir())
+    assert(set(tmpdir_path.iterdir()) == {tmpdir_path / 'df.txt'})
+    assert((tmpdir_path / 'df.txt').read_text() == 'roolz')
